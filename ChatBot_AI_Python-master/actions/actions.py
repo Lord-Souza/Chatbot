312c0fdac2e9b91ae232d232aeac733cb01374f9
Lord-Souza <104236211+Lord-Souza@users.noreply.github.com>
2024-03-05 11:38:10 -0300

    Chat retorna objetos

--- ChatBot_AI_Python-master/actions/actions.py
+++ ChatBot_AI_Python-master/actions/actions.py
@@ -1,26 +1,73 @@
-import json
-import logging
+from dataclasses import dataclass
+from typing import List, Dict, Any, Text
 import datetime
+import re
+import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-import requests
-from typing import Any, Text, Dict, List, Union, Tuple
-import re
+
+@dataclass
+class ChatResponse:
+    message: str
+    action: str
+    entities: Dict[Text, Any]
 
 class ActionGetAtendimentos(Action):
     def name(self) -> Text:
         return "action_get_atendimentos"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[ChatResponse]:
         try:
             api_url = "http://localhost:8080/bot/getAtendimentos"
             response = requests.get(api_url)
             response.raise_for_status()  # Verificar se houve algum erro na solicitação
             atendimentos = response.json()
 
-            # Identificar datas na mensagem do usuário usando expressão regular
+            # Identificar o convênio na mensagem do usuário usando expressão regular
             message_text = tracker.latest_message.get('text')
+            convenio_intervalo_datas = re.search(r'Mostre os atendimentos do (.+) entre (\d{1,2}[/-]\d{1,2}[/-]\d{2,4}) e (\d{1,2}[/-]\d{1,2}[/-]\d{2,4})', message_text)
+
+            if convenio_intervalo_datas:
+                convenio = convenio_intervalo_datas.group(1).lower()
+                data_inicial = convenio_intervalo_datas.group(2)
+                data_final = convenio_intervalo_datas.group(3)
+
+                # Formatando as datas de referência
+                data_inicial_formatada = self._formatar_data(data_inicial)
+                data_final_formatada = self._formatar_data(data_final)
+
+                # Filtrar os atendimentos pelo convênio e intervalo de datas especificados
+                atendimentos_filtrados = [atendimento for atendimento in atendimentos if convenio in atendimento.get("dsConvenio").lower() and self._comparar_datas_intervalo(atendimento.get("dtEntrada"), data_inicial_formatada, data_final_formatada)]
+
+                if atendimentos_filtrados:
+                    dispatcher.utter_message(f"Atendimentos do convênio '{convenio}' entre {data_inicial_formatada} e {data_final_formatada}: {len(atendimentos_filtrados)}\n")
+                    for atendimento in atendimentos_filtrados:
+                        formatted_info = self._formatar_atendimento(atendimento)
+                        dispatcher.utter_message(formatted_info)
+                else:
+                    dispatcher.utter_message(f"Atendimentos do convênio '{convenio}' entre {data_inicial_formatada} e {data_final_formatada} não encontrados.")
+                return [ChatResponse(message=dispatcher.messages, action="action_get_atendimentos", entities={})]
+
+            # Identificar o convênio na mensagem do usuário usando expressão regular
+            convenio_especifico = re.search(r'Mostre os atendimentos do (.+)', message_text)
+
+            if convenio_especifico:
+                convenio = convenio_especifico.group(1).lower()
+
+                # Filtrar os atendimentos pelo convênio especificado
+                atendimentos_convenio = [atendimento for atendimento in atendimentos if convenio in atendimento.get("dsConvenio").lower()]
+
+                if atendimentos_convenio:
+                    dispatcher.utter_message(f"Atendimentos do convênio '{convenio}': {len(atendimentos_convenio)}\n")
+                    for atendimento in atendimentos_convenio:
+                        formatted_info = self._formatar_atendimento(atendimento)
+                        dispatcher.utter_message(formatted_info)
+                else:
+                    dispatcher.utter_message(f"Atendimentos do convênio '{convenio}' não encontrados.")
+                return [ChatResponse(message=dispatcher.messages, action="action_get_atendimentos", entities={})]
+
+            # Identificar datas na mensagem do usuário usando expressão regular
             datas_encontradas = re.findall(r'\b\d{1,2}[/-]\d{1,2}[/-]\d{2,4}\b', message_text)
 
             if datas_encontradas:
@@ -88,9 +135,20 @@ class ActionGetAtendimentos(Action):
                                     dispatcher.utter_message(f"Total de atendimentos na Clínica {nome_clinica}: {numeros_atendimentos}")
                                     for atendimento in atendimentos_clinica:
                                         formatted_info = self._formatar_atendimento(atendimento)
-                                        dispatcher.utter_message(formatted_info + '\n')
+                                        dispatcher.utter_message(formatted_info)
                                 else:
                                     dispatcher.utter_message(f"Atendimentos na Clínica {nome_clinica} não encontrados.")
+                            elif entity['entity'] == 'dsConvenio':
+                                convenio = entity['value']
+                                atendimentos_convenio = [atendimento for atendimento in atendimentos if atendimento.get("dsConvenio") == convenio]
+                                if atendimentos_convenio:
+                                    numeros_atendimentos = len(atendimentos_convenio)
+                                    dispatcher.utter_message(f"Total de atendimentos com o convênio {convenio}: {numeros_atendimentos}")
+                                    for atendimento in atendimentos_convenio:
+                                        formatted_info = self._formatar_atendimento(atendimento)
+                                        dispatcher.utter_message(formatted_info)
+                                else:
+                                    dispatcher.utter_message(f"Atendimentos com o convênio {convenio} não encontrados.")
                             # Adicione aqui outras condições para outras entidades, se necessário
                     
                     elif 'quantos atendimentos' in message_text and 'esta semana' in message_text:
@@ -105,23 +163,24 @@ class ActionGetAtendimentos(Action):
                         total_atendimentos = len(atendimentos)
                         dispatcher.utter_message(f"Total de atendimentos: {total_atendimentos}")
                     else:
-                        dispatcher.utter_message("Por favor, forneça o número do atendimento, o nome do médico, do paciente, da clínica ou a data de referência.")
+                        dispatcher.utter_message("Por favor, forneça o número do atendimento, o nome do médico, do paciente, da clínica ou o convênio.")
         except requests.exceptions.RequestException as e:
             dispatcher.utter_message("Ocorreu um erro ao obter os dados da API.")
         except Exception as e:
             dispatcher.utter_message("Ocorreu um erro ao processar a solicitação.")
 
-        return []
+        return [ChatResponse(message=dispatcher.messages, action="action_get_atendimentos", entities={})]
 
     def _formatar_atendimento(self, atendimento: Dict[str, Any]) -> str:
         formatted_info = "\n".join([
-            f"Nº do Atendimento: {atendimento['nrAtendimento']}",
-            f"Médico: {atendimento['nmMedico']}",
+            f"Nº do Atendimento: {atendimento['nrAtendimento']}",            
             f"Paciente: {atendimento['nmPaciente']}",
+            f"Médico: {atendimento['nmMedico']}",
             f"Data de Entrada: {atendimento['dtEntrada']}",
             f"Convênio: {atendimento['dsConvenio']}",
-            f"Clínica: {atendimento['dsClinica']}"
-        ])
+            f"Clínica: {atendimento['dsClinica']}",
+            " "  # Adicionando uma quebra de linha no final
+        ]) + '\n\n'
         return formatted_info
 
     def _formatar_data(self, data: str) -> str:
@@ -134,6 +193,9 @@ class ActionGetAtendimentos(Action):
 
     def _comparar_datas(self, data1: str, data2: str) -> bool:
         return data1.startswith(data2)
+
+    def _comparar_datas_intervalo(self, data: str, data_inicial: str, data_final: str) -> bool:
+        return data_inicial <= data <= data_final
 
     def _esta_semana(self, data: str) -> bool:
         if not data:
